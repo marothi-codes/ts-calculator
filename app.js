e21a2d9148c4c3e4459128a2e9913da74963e2db
Marothi Mahlake <code_artist@outlook.com>
2022-05-10 11:13:56 +0200

    TypeScript CLI calculator finished.

--- app.js
+++ app.js
@@ -2,6 +2,47 @@
 exports.__esModule = true;
 var readline_sync_1 = require("readline-sync");
 function main() {
-    console.log(readline_sync_1.question);
+    var firstString = (0, readline_sync_1.question)('Enter first number.\n');
+    var operator = (0, readline_sync_1.question)('Enter operator.\n');
+    var secondString = (0, readline_sync_1.question)('Enter second number.\n');
+    if (isNumber(firstString) && isOperator(operator) && isNumber(secondString)) {
+        var firstNum = parseFloat(firstString);
+        var secondNumber = parseFloat(secondString);
+        var result = calculate(firstNum, secondNumber, operator);
+        console.log(result);
+        main();
+    }
+    else {
+        console.log('\ninvalid input\n');
+        main();
+    }
+}
+function isNumber(input) {
+    var maybeNum = parseInt(input);
+    var isNum = !isNaN(maybeNum);
+    return isNum;
+}
+function isOperator(input) {
+    switch (input) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return true;
+        default:
+            return false;
+    }
+}
+function calculate(operandX, operandY, operator) {
+    switch (operator) {
+        case '+':
+            return operandX + operandY;
+        case '-':
+            return operandX - operandY;
+        case '*':
+            return operandX * operandY;
+        case '/':
+            return operandX / operandY;
+    }
 }
 main();
--- app.ts
+++ app.ts
@@ -1,7 +1,54 @@
-import {question} from 'readline-sync';
+import { question } from 'readline-sync';
+
+type Operator = '+' | '-' | '*' | '/';
 
 function main() : void {
-  console.log(question);
+  const firstString: string = question('Enter first number.\n');
+  const operator: string = question('Enter operator.\n');
+  const secondString: string = question('Enter second number.\n');
+
+  if (isNumber(firstString) && isOperator(operator) && isNumber(secondString)) {
+    const firstNum: number = parseFloat(firstString);
+    const secondNumber: number = parseFloat(secondString);
+    const result = calculate(firstNum, secondNumber, operator as Operator);
+    console.log(result);
+    console.log('\nPress Ctrl + C to exit.\n')
+    main();
+  } else {
+    console.log('\ninvalid input\n');
+    main();
+  }
 }
+
+function isNumber(input: string): boolean {
+  const maybeNum = parseInt(input);
+  const isNum: boolean = !isNaN(maybeNum);
+  return isNum;
+}
+
+function isOperator(input: string): boolean {
+  switch(input) {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+      return true;
+    default:
+      return false;
+  }
+}
+
+function calculate(operandX: number, operandY: number, operator: Operator): number {
+  switch (operator) {
+    case '+':
+      return operandX + operandY;
+    case '-':
+      return operandX - operandY;
+    case '*':
+      return operandX * operandY;
+    case '/':
+      return operandX / operandY;
+    }
+  }
 
 main();
